4810c59875b4a3d94825b09f44f657dd191ab10c
Jawaun Brown <jawaun.brown95@gmail.com>
2021-06-15 23:07:36 -0400

    Create Simplified4QuadruplebjectPrimer.js

---
+++ Simplified4QuadruplebjectPrimer.js
@@ -0,0 +1,30 @@
+// QUADRUPLE OBJECTS: The White House
+
+const realQualities = {
+	color: "white	,
+	age: "287",
+	country: "america",
+	: {},
+};
+
+const realWhiteHouse = {realObject, ...realQualities};
+
+const sensualWhiteHouse = {{realWhiteHouse} ...sensualQualities};
+
+const whiteHouse = {{realWhiteHouse}, sensualWhiteHouse};
+
+
+//based on our understanding of 4-objects, it seems like the white house example would point to something like this
+const {essence, eidos, space, time} = {realWhiteHouse, {{ ...realQualities}}, whiteHouse,  {...sensualQualities}};
+
+const quadrupleObjectWhiteHouse = {
+
+	essence: realWhiteHouse + real object 
+
+	eidos: {{ ...realQualities}},
+
+	space: whiteHouse,
+
+	time: {...sensualQualities},
+
+}
